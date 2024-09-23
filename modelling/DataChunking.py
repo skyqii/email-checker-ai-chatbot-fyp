10e65b2c17dd3cb817aa414942dcada682462ffa
skyqii <140032908+skyqii@users.noreply.github.com>
2024-09-23 17:25:43 +0800

    modified:   modelling/DataChunking.py

--- modelling/DataChunking.py
+++ modelling/DataChunking.py
@@ -16,7 +16,7 @@ load_dotenv()
 #---- Set OpenAI API key 
 # Change environment variable name from "OPENAI_API_KEY" to the name given in 
 # your .env file.
-openai.api_key = ''
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "modelling\data"
